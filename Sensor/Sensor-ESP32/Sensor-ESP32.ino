3c9b80c8f6f50dbe56c16f89f3ba496bbe9d4986
Gus G <gus.gran@gmail.com>
2023-09-30 10:34:24 -0700

    Figure out how to integrate in network connectivity and JSON to upload measurements to the server

--- Sensor/Sensor-ESP32/Sensor-ESP32.ino
+++ Sensor/Sensor-ESP32/Sensor-ESP32.ino
@@ -4,7 +4,14 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_MPL3115A2.h>
 
+#include <ArduinoJson.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+
 #include "CDSensor.h"
+#include "NetworkConfig.h"
+
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -18,8 +25,19 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Adafruit_MPL3115A2 baro;
 
+// Timing for saving the data. Saves every 10 minutes -- 144 measurements / day.
+const unsigned long DelayIntervalMs = 5000; // 5 seconds
+const unsigned long DelayIntervalsBeforeReporting = (10 * 60 * 1000) / DelayIntervalMs;
+unsigned long delayIntervalsRemaining = 0;
+
+float lastPressure;
+float lastAltitude;
+float lastTemperature;
+int lastCo2;
+
 void setup() {
   Wire.begin(); // Setup display / sensor I2C
+  Serial.begin(9600); // For diagnostics
 
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
@@ -48,9 +66,62 @@ void loop() {
   drawMeasurement();
 
   // Flash the LED for a half-second after each measurement update
-  flashLED();
+  flashLED(500);
 
-  delay(2000);
+  // Connect to network and save measurement
+  if (delayIntervalsRemaining == 0)
+  {
+    Serial.println("Uploading to network...");
+    enableWifi();
+
+    // Compute JSON document of measurements and upload.
+    StaticJsonDocument<200> doc;
+    doc["pressureHpa"] = lastPressure;
+    doc["altitudeFt"] = lastAltitude;
+    doc["temperatureC"] = lastTemperature;
+    doc["co2Ppm"] = lastCo2;
+    
+    String json;
+    serializeJson(doc, json);
+    serializeJson(doc, Serial);
+
+    WiFiClientSecure client;
+    client.setInsecure();
+
+    HTTPClient http;
+    http.begin(client, SAVE_API);
+    http.addHeader("Content-Type", "application/json");
+
+    int httpCode = http.POST(json);
+    Serial.println(httpCode);
+
+    http.end();
+
+    delayIntervalsRemaining = DelayIntervalsBeforeReporting;
+    disableWiFi();
+  }
+  else
+  {
+    delayIntervalsRemaining--;
+    Serial.println(delayIntervalsRemaining);
+  }
+
+  delay(DelayIntervalMs);
+}
+
+void enableWifi() {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, SSID_PASSWORD);
+
+    // Wait for network connection
+    while (WiFi.status() != WL_CONNECTED) {
+      flashLED(250);
+    }
+}
+
+void disableWiFi() {
+  WiFi.disconnect();
+  WiFi.mode(WIFI_OFF);
 }
 
 int lastX = 0;
@@ -73,15 +144,15 @@ void drawMeasurement() {
   }
   display.setCursor(displayOffset, 24);
 
-  int measurement = GetCO2PPM();
-  display.print(measurement);
+  lastCo2 = GetCO2PPM();
+  display.print(lastCo2);
   display.println(F(SENSOR_UNITS));
   display.display();
 
   // Chart measurement: Because 36-64 is not erased, persist pixels in there for charting/
   // Measurement values are from ~400 to 2000. Squash that 16,000 range into the 27 pixels available
   // (apx 60 ppm per pixel change). 
-  int adjustedMeasurement = (SCREEN_HEIGHT - ((measurement / 60) - 6)); // Approximately now from 0 to 27
+  int adjustedMeasurement = (SCREEN_HEIGHT - ((lastCo2 / 60) - 6)); // Approximately now from 0 to 27
   adjustedMeasurement = fitToRange(adjustedMeasurement, CHART_START + 1, SCREEN_HEIGHT - 2);
   if (lastY == -1) {
     lastY = adjustedMeasurement; // First-time fix so that point-to-point lines start fine
@@ -124,31 +195,31 @@ void drawDeviceInfo() {
   unsigned int firmwareVersion = GetFirmwareVersion();
   bool isDeviceHealthy = IsDeviceHealthy();
 
-   float pressure = baro.getPressure();
-  float altitude = baro.getAltitude();
-  float temperature = baro.getTemperature();
+  lastPressure = baro.getPressure();
+  lastAltitude = baro.getAltitude()/3.2808;
+  lastTemperature = ((baro.getTemperature() * 9.0f) / 5.0f) + 32.0f;
 
   display.print(F(SENSOR_NAME));
   display.print(isDeviceHealthy ? "+ " : "- ");
-  display.print(pressure);
+  display.print(lastPressure);
   display.println(" hPa");
 
   display.print(F("S: "));
   display.print(serialNumber);
   display.print(" ");
-  display.printf("%.1f", altitude/3.2808);
+  display.printf("%.1f", lastAltitude);
   display.println(" ft");
 
   display.print(F("F: "));
   display.print(firmwareVersion);
   display.print(" ");
-  display.printf("%.1f", ((temperature * 9.0f) / 5.0f) + 32.0f);
+  display.printf("%.1f", lastTemperature);
   display.println(" F");
 }
 
-void flashLED() {
+void flashLED(unsigned long delayMs) {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
+  delay(delayMs);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+  delay(delayMs);
 }
