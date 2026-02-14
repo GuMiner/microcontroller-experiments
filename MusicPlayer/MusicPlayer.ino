6e677ec7ddebedc9860e6e8bf0affe9a0f860e0a
GG <gus.gran@gmail.com>
2026-02-13 21:28:10 -0800

    Clean up code a bit, begin to investigate NFC interrupts

--- MusicPlayer/MusicPlayer.ino
+++ MusicPlayer/MusicPlayer.ino
@@ -6,100 +6,96 @@
 
 // Full wiring diagram:
 /*
-// ###### I2C, PN532 NFC ######
+// As a note, in the UNO R4 Minima/Nano V3 CS == SSLB, MOSI == COPI, MISO == CIPO, CLK == RSPCK
+
+// ###### SPI, PN532 NFC ######
 5V -- NFC (VCC)
 GND -- NFC (GND)
+
 D5 --- NFC (IRQ)
 D6 --- NFC (RESET)
-SDA --- NFC (SDA)
-SCL --- NFC (SCL)
 
-// Because the UNO R4 Minima doesn't include I2C pullup resistors:
-SDA --- 4.7k Ω --- 5 V
-SCL --- 4.7k Ω --- 5 V
-
-// By using SDA and SCL on this Arduino, A4 and A5 are also in use
-A4 --- SDA
-A5 --- SCL
+D10 --- VS1053 (CS)
+D11 --- VS1053 (MOSI)
+D12 --- VS1053 (MISO)
+D13 --- VS1053 (CLK)
 
 // ###### SPI, VS1053 ######
-
 5V -- VS1053 (VCC)
 GND -- VS1053 (GND)
 
 D3 --- VS1053 (DREQ)
 D4 --- VS1053 (SDCS)
-D8 --- VS1053 (XDCS)
+D7 --- VS1053 (XDCS)
+D8 --- VS1053 (RST)
 
-// As a note, in the UNO R4 Minima CS == SSLB, MOSI == COPI, MISO == CIPO, CLK == RSPCK
-D9 --- VS1053 (RST)
-D10 --- VS1053 (CS)
+D9 --- VS1053 (CS)
 D11 --- VS1053 (MOSI)
 D12 --- VS1053 (MISO)
 D13 --- VS1053 (CLK)
 
 */
 
-// TODO -- solder and wire these, as IRQ (interrupts) will be valuable
-#define PN532_IRQ   5
-#define PN532_RESET 6  // Not connected by default on the NFC Shield
-#define PN532_CHIP_SELECT 10  // The 'SS' pin
+#define PN532_IRQ   5 // For interrupts, but used as a manual digital input
+#define PN532_RESET 6 
+#define PN532_CHIP_SELECT 10
 
-#define DREQ 3      // VS1053 Data request, ideally an Interrupt pin
-#define CARDCS 4     // Card chip select pin
+#define VS1053_DREQ 3      // VS1053 Data request, on an interrupt pin
+#define VS1053_CARDCS 4     // Card chip select pin
 // DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
-#define BREAKOUT_DCS    7      // VS1053 Data/command select pin (output)
-#define BREAKOUT_RESET  8      // VS1053 reset pin (output)
-#define BREAKOUT_CS    9     // VS1053 chip select pin (output)
-// THESE CHIP SELECT PINS NEE
+#define VS1053_BREAKOUT_DCS    7      // VS1053 Data/command select pin (output)
+#define VS1053_BREAKOUT_RESET  8      // VS1053 reset pin (output)
+#define VS1053_BREAKOUT_CS    9     // VS1053 chip select pin (output)
 
+// Both use hardware SPI
 Adafruit_PN532 nfc(PN532_CHIP_SELECT);
-
-Adafruit_VS1053_FilePlayer musicPlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
+Adafruit_VS1053_FilePlayer musicPlayer(VS1053_BREAKOUT_RESET, VS1053_BREAKOUT_CS, VS1053_BREAKOUT_DCS, VS1053_DREQ, VS1053_CARDCS);
 
 void setup(void) {
-  pinMode(LED_BUILTIN, OUTPUT); // Shared with SPI pin, weird. Doens't work well then though.
+  Serial.begin(115200);
 
-
-Serial.begin(115200);
- while (!Serial) delay(10); // for Leonardo/Micro/Zero
-
-  Serial.println("Hello!");
+  // For debugging, halt until a serial connection is established
+  while (!Serial) delay(10); // for Leonardo/Micro/Zero
 
   nfc.begin();
-
   uint32_t versiondata = nfc.getFirmwareVersion();
-  if (! versiondata) {
+  if (!versiondata) {
     Serial.print("Didn't find PN53x board");
-    while (1); // halt
+    while (1);
   }
+
+  nfc.SAMConfig();
+  pinMode(PN532_IRQ, INPUT_PULLUP);
+
   // Got ok data, print it out!
   Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX);
-  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC);
+  Serial.print("Firmware ver. "); Serial.println((versiondata>>16) & 0xFF, DEC);
 
 
-  if (! musicPlayer.begin()) { // initialise the music player
-     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+  if (!musicPlayer.begin()) { // initialise the music player
+     Serial.println(F("Didn't find VS1053"));
      while (1);
   }
   Serial.println(F("VS1053 found"));
  
-   if (!SD.begin(CARDCS)) {
+   if (!SD.begin(VS1053_CARDCS)) {
     Serial.println(F("SD failed, or not present"));
-    while (1);  // don't do anything more
+    while (1);
   }
 
   // list files
   printDirectory(SD.open("/"), 0);
   
   // Set volume for left, right channels. lower numbers == louder volume!
-  musicPlayer.setVolume(20,20);
+  musicPlayer.setVolume(80,80);
 
-   musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT); 
-     // Play another file in the background, REQUIRES interrupts!
+  musicPlayer.useInterrupt(VS1053_FILEPLAYER_PIN_INT); 
   Serial.println(F("Playing track"));
   musicPlayer.startPlayingFile("/test1.mp3");
-  
+
+  nfc.startPassiveTargetIDDetection(PN532_MIFARE_ISO14443A);
+  attachInterrupt(digitalPinToInterrupt(PN532_IRQ), cardDetectedInterrupt, FALLING); // TODO need to rewire IRQ to pin 2, because that's the only digital interrupt on the nano. 
+  // The '~' symbol means PWM, not interrupt.
 }
 
 uint8_t readAsASCII(uint8_t* data, int idx) {
@@ -130,19 +126,14 @@ int8_t readTagNumber() {
   return result;
 }
 
-void loop(void) {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on
-  delay(500);                      // wait for a half second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off
-  delay(500);  
-
+void readCard() {
   uint8_t success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
 
   // Wait for an NTAG203 card.  When one is found 'uid' will be populated with
   // the UID, and uidLength will indicate the size of the UUID (normally 7)
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 500); // 0.5s timeout
+  success = nfc.readDetectedPassiveTargetID(uid, &uidLength);
 
   if (success) {
     // Display some basic information about the card
@@ -155,6 +146,23 @@ void loop(void) {
     Serial.println(readTagNumber(), DEC);
   }
 }
+
+volatile bool hasCard = false;
+int nfcInterruptState = HIGH;
+
+void loop() {
+  delay(20);
+  if (hasCard) {
+    readCard();
+    hasCard = false;
+  }
+}
+
+void cardDetectedInterrupt() {
+  hasCard = true;
+  Serial.println("Detected card");
+}
+
 
 /// File listing helper
 void printDirectory(File dir, int numTabs) {
